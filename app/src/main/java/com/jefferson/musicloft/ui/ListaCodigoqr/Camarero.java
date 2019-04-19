071d1be0f19026cd258e67c939f751d32a484cff
Jefferson Max Tomala Villarreal <jtv215@inlumine.ual.es>
2019-04-20 00:41:40 +0200

    Se modifica ventana lista de codigo QR y mostrar codigo (Fronted)

--- app/src/main/java/com/jefferson/musicloft/ui/ListaCodigoqr/Camarero.java
+++ app/src/main/java/com/jefferson/musicloft/ui/ListaCodigoqr/Camarero.java
@@ -1,12 +1,26 @@
 package com.jefferson.musicloft.ui.ListaCodigoqr;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jefferson.musicloft.R;
+import com.jefferson.musicloft.common.SharedPreferencedManager;
+import com.jefferson.musicloft.ui.DashboardActivity;
 import com.jefferson.musicloft.ui.ListaCodigoqr.CodigoListFragment;
+import com.jefferson.musicloft.ui.inicio.MainActivity;
 
 public class Camarero extends AppCompatActivity {
+
+    Toolbar toolbar;
+    ImageView fotoPerfil,salir;
+    TextView nombreLocal,puntos,tituloPuntos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,28 @@ public class Camarero extends AppCompatActivity {
                 .add(R.id.fragmentContainerCodigoQR, new CodigoListFragment())
                 .commit();
 
+        toolbar = findViewById(R.id.toolbar);
+        nombreLocal = findViewById(R.id.nombreLocalID);
+        puntos = findViewById(R.id.puntosID);
+        fotoPerfil = findViewById(R.id.fotoPerfilID);
+        tituloPuntos = findViewById(R.id.tituloPuntosID);
 
+        puntos.setVisibility(View.INVISIBLE);
+        tituloPuntos.setVisibility(View.INVISIBLE);
+
+
+        salir = findViewById(R.id.btnSalir);
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Camarero.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        nombreLocal.setText("Camarero");
 
     }
 }
--- app/src/main/java/com/jefferson/musicloft/ui/ListaCodigoqr/MostrarCodigoQR.java
+++ app/src/main/java/com/jefferson/musicloft/ui/ListaCodigoqr/MostrarCodigoQR.java
@@ -3,17 +3,24 @@ package com.jefferson.musicloft.ui.ListaCodigoqr;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.jefferson.musicloft.R;
+import com.jefferson.musicloft.common.SharedPreferencedManager;
+import com.jefferson.musicloft.ui.inicio.MainActivity;
 
 public class MostrarCodigoQR extends AppCompatActivity {
 
     ImageView image;
     Button btnVolverAtras;
+    Toolbar toolbar;
+    ImageView fotoPerfil,salir;
+    TextView nombreLocal,puntos,tituloPuntos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +36,27 @@ public class MostrarCodigoQR extends AppCompatActivity {
     private void findViewById() {
         image = findViewById(R.id.imageCodigoQRID);
         btnVolverAtras = findViewById(R.id.btnVolver);
+        toolbar = findViewById(R.id.toolbar);
+        nombreLocal = findViewById(R.id.nombreLocalID);
+        puntos = findViewById(R.id.puntosID);
+        fotoPerfil = findViewById(R.id.fotoPerfilID);
+        tituloPuntos = findViewById(R.id.tituloPuntosID);
+
+        puntos.setVisibility(View.INVISIBLE);
+        tituloPuntos.setVisibility(View.INVISIBLE);
+
+        salir = findViewById(R.id.btnSalir);
+
+        salir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MostrarCodigoQR.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        nombreLocal.setText("Camarero");
     }
 
     private void cargardatos() {
